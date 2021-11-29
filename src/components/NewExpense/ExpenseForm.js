4fe24897bcc39f0b2540c95a37fe8ca693a78712
abbasmansoori <abbas.mansoori@hotmail.com>
2021-11-30 00:25:36 +0100

    added form

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -16,7 +16,7 @@ const ExpenseForm = () => {
         </div>
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" min="2019-01-01" step="2022-01-01" />
+          <input type="date" min="2019-01-01" max="2022-01-01" />
         </div>
         </div>
         <div className="new-expense__actions">
