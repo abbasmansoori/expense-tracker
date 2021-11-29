94d9ec1f4715a53760087543d5081915d85fe655
abbasmansoori <abbas.mansoori@hotmail.com>
2021-11-29 23:48:27 +0100

    added form

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Expenses from './components/Expenses/Expenses';
+import NewExpense from './components/NewExpense/NewExpense';
 import "./App.css"
 const App = () => {
   const expenses = [
@@ -26,6 +27,7 @@ const App = () => {
   return (
     <div className="App">
       <h2>Expense Tracker!</h2>
+      <NewExpense />
       <Expenses items={expenses} />
     </div>
   );
